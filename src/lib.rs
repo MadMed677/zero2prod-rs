e5067db4411a09111b15de3e796f1b1b04d15484
Artem Anashev <madmed677@gmail.com>
2022-01-17 23:53:58 +0500

    Run tests on a random port based on `port 0`

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
+use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
+use std::net::TcpListener;
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
@@ -7,11 +9,12 @@ async fn health_check() -> HttpResponse {
 // We need to mark `run` as public.
 // It is no longer a binary entrypoint, therefore we can mark it as async
 // without having to use any proc-macro incantation.
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .listen(listener)?
+        .run();
+
+    Ok(server)
 }
 
 #[cfg(test)]
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,15 +1,25 @@
-async fn spawn_app() -> std::io::Result<()> {
-    todo!()
+use std::net::TcpListener;
+
+/// Returns an address of spawning app
+fn spawn_app() -> String {
+    // Spawn our app on `Port 0`. Which means that it will be
+    //  generated automatically
+    // See: https://www.lifewire.com/port-0-in-tcp-and-udp-818145
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
 
-#[actix_rt::test]
+#[tokio::test]
 async fn health_check_works() {
-    spawn_app().await.expect("Failed to spawn our app.");
-
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
