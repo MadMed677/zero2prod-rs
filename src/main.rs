77b78d439a0c60aa5af245d7ee33fea32bac865f
Artem Anashev <madmed677@gmail.com>
2022-01-23 12:00:26 +0500

    Print logs for our application

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+
 use zero2prod::configuration::get_configuration;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // Redirect all `log`'s events to our subscriber
+    LogTracer::init().expect("Failed to set logger");
+
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    set_global_default(subscriber).expect("Failed to set subscriber");
+
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = sqlx::PgPool::connect(&configuration.database.connection_string())
